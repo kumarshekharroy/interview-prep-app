4edf6250d40fadb5f54b761abbd640bc80c25517
Shekhar Roy <mr.shekharroy@gmail.com>
2026-07-07 22:09:42 +0530

    fixed base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 import process from 'node:process' 
 
 const pagesBase =
-  process.env.GITHUB_PAGES === 'true' ? '/interview-prep-plan/' : '/';
+  process.env.GITHUB_PAGES === 'true' ? '/interview-prep-app/' : '/';
 
 export default defineConfig({
   base: pagesBase,
